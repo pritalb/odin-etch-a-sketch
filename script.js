7b345d1a217bf05e7ca6e822d6e414ce3c987cfa
pritalb <pritalbamnodkar@gmail.com>
2022-05-10 00:07:58 +0530

    changing the value of the slider changes the size of the grid.

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function CreateGrid() {
 
 	const cell = document.createElement('div');
 	const grid_size = document.querySelector('#slider').value;
-	const cell_size = round(600/grid_size, 2); // 600px is the canvas width/height
+	const cell_size = round(600/grid_size, 4); // 600px is the canvas width/height
 	const cell_size_formatted = `${cell_size}px`
 	cell.className = 'cell';
 	cell.style.height = cell_size_formatted;
@@ -39,29 +39,6 @@ function CreateGrid() {
 	for (let index = 0; index < grid_size; index++) {
 		grid.append(row.cloneNode(true));
 	}
-}
-
-
-function changeColor(cell) {
-	if (canDraw) {
-		// console.log('change color function called.');
-		let color = document.querySelector('#color-picker').value;;
-
-		if (canErase) {
-			color = '#ffffff';
-		}
-	
-		cell.style.backgroundColor = color;
-	}
-}
-
-function toggleEraser() {
-	canErase = !canErase;
-}
-
-function main() {
-	CreateGrid();
-	document.querySelector('#slider').addEventListener('change', CreateGrid);
 
 	document.querySelectorAll('.cell').forEach(cell => {
 		cell.addEventListener('mousedown', (event) => {
@@ -80,6 +57,35 @@ function main() {
 
 			changeColor(cell);
 		})
+	});
+
+}
+
+
+function changeColor(cell) {
+	if (canDraw) {
+		console.log('change color function called.');
+		let color = document.querySelector('#color-picker').value;;
+
+		if (canErase) {
+			color = '#ffffff';
+		}
+	
+		cell.style.backgroundColor = color;
+	}
+}
+
+function toggleEraser() {
+	canErase = !canErase;
+}
+
+function main() {
+	CreateGrid();
+
+	const slider = document.querySelector('#slider');
+	slider.addEventListener('change', () => {
+		document.querySelector('#slider_label').innerHTML = slider.value;
+		CreateGrid();
 	});
 
 	const eraserButton = document.querySelector('#eraser');
